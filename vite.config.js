09b1f866d001791a6020a52d24f7d10e9c613001
lucasn.panadero <lucasn.panadero@gmail.com>
2024-07-02 04:00:04 +0000

    PWA

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,50 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      manifest: {
+        name: 'Rollytone',
+        short_name: 'Rollytone',
+        description: 'Rollytone es una red social que busca ser una plataforma de intercambio de gustos y opiniones entre personas amantes de la música.',
+        theme_color: '#FD703A',
+        background_color: '#282828',
+        display: 'standalone',
+        display_override: ['window-controls-overlay'],
+        lang: 'es-ES',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
